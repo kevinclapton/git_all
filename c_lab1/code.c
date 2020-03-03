cb569c7fae1aa5689733a94f10e92f07ee0bd943
Semyon Varlamov <sem.varlamow@mail.ru>
2020-03-03 19:58:28 +0700

    v0.02

--- c_lab1/code.c
+++ c_lab1/code.c
@@ -3,12 +3,18 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+	char str[254];
+	size_t len;
+	int i=0;
 	FILE *inout;
 	if (!(inout=fopen(argv[1],  "r")))
 		return 1;
 	printf("Файл %s открыт...\n",argv[1]);
-	/*while (feof(inout)) {
-		while (*/
+	while (!feof(inout)) {
+		fgets(&str, "%s", inout);
+		printf("Строка №%d: %s\n", i+1, str);
+		i++;
+	}
 	fclose(inout);
 	printf("Файл %s закрыт.\n", argv[1]);
 	return 0;
